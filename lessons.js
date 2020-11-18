90098a4f6042fe8ce0e0fbb5a535152dfda6cac7
aishaar <aishaar283@gmail.com>
2020-11-18 18:24:29 +0400

    js file containing list of lessons

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        id: 101,
+        subject: "Biology",
+        location: "Sharjah",
+        price: 150,
+        availableSpaces : 5,
+        image: "Images/biology.png"
+    },
+    {
+        id: 102,
+        subject: "Maths",
+        location: "Dubai",
+        price: 200,
+        availableSpaces : 5,
+        image: "Images/maths.png"
+        
+    },
+    {
+        id: 103,
+        subject: "English",
+        location: "Abu Dhabi",
+        price: 100,
+        availableSpaces : 5,
+        image: "Images/english.jpg"
+       
+    },
+    {
+        id: 104,
+        subject: "Physics",
+        location: "Ajman",
+        price: 75,
+        availableSpaces : 5,
+        image: "Images/physics.jpg"
+       
+    },
+    {
+        id: 105,
+        subject: "Chemistry",
+        location: "Sharjah",
+        price: 99,
+        availableSpaces : 5,
+        image: "Images/chemistry.png"
+        
+    },
+    {
+        id: 106,
+        subject: "Psychology",
+        location: "Dubai",
+        price: 50,
+        availableSpaces : 5,
+        image: "Images/psychology.jpg"
+        
+    },
+    {
+        id: 107,
+        subject: "Business",
+        location: "Abu Dhabi",
+        price: 100,
+        availableSpaces : 5,
+        image: "Images/business.jpg"
+        
+    },
+    {
+        id: 108,
+        subject: "History",
+        location: "Ajman",
+        price: 60,
+        availableSpaces : 5,
+        image: "Images/history.jpg"
+        
+    },
+    {
+        id: 109,
+        subject: "Computer Science",
+        location: "Fujairah",
+        price: 100,
+        availableSpaces : 5,
+        image: "Images/computer.png"
+        
+    },
+    {
+        id: 110,
+        subject: "Arabic",
+        location: "Dubai",
+        price: 85,
+        availableSpaces : 5,
+        image: "Images/arabic.jpg"
+        
+    }
+
+]
